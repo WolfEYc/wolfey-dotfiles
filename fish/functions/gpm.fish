046b979d7f935a8b310410e4a66ff5c66a1452a3
WolfEYc <wolfeycode@gmail.com>
2024-09-29 14:01:45 -0500

    move to the best programming lang, rust (obviously)

--- fish/functions/gpm.fish
+++ fish/functions/gpm.fish
@@ -1,3 +1,3 @@
-function gpm --wraps='fd . ~/Code --exact-depth 1 -td | xargs -I {} basename {} | fzf | xargs -I {} code ~/Code/{}'
-    fd . ~/Code --exact-depth 1 -td | xargs -I {} basename {} | fzf | xargs -I {} code ~/Code/{}
+function gpm --wraps='fd . ~/Code --exact-depth 1 -td | xargs -I {} basename {} | sk | xargs -I {} code ~/Code/{}'
+    fd . ~/Code --exact-depth 1 -td | xargs -I {} basename {} | sk | xargs -I {} code ~/Code/{}
 end
--- fish/functions/gpmr.fish
+++ fish/functions/gpmr.fish
@@ -1,3 +1,3 @@
-function gpmr --wraps='fd . ~/Code --exact-depth 1 -td | xargs -I {} basename {} | fzf | xargs -I {} code ~/Code/{} -r'
-    fd . ~/Code --exact-depth 1 -td | xargs -I {} basename {} | fzf | xargs -I {} code ~/Code/{} -r
+function gpmr --wraps='fd . ~/Code --exact-depth 1 -td | xargs -I {} basename {} | sk | xargs -I {} code ~/Code/{} -r'
+    fd . ~/Code --exact-depth 1 -td | xargs -I {} basename {} | sk | xargs -I {} code ~/Code/{} -r
 end
